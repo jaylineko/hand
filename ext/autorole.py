f635bb0997c9b5660eef79d12e8ddb092c478be9
vivi <me@vivi.fyi>
2020-06-05 23:23:58 +0100

    Add bot autorole

--- ext/autorole.py
+++ ext/autorole.py
@@ -26,6 +26,12 @@ class Autorole(commands.Cog):
             name="Default member role",
             value=f"<@&{config['autorole']}>" if "autorole" in config else "None",
         )
+        embed.add_field(
+            name="Default bot role",
+            value=f"<@&{config['autorole-bot']}>"
+            if "autorole-bot" in config
+            else "None",
+        )
 
         await ctx.send(embed=embed)
 
@@ -46,11 +52,30 @@ class Autorole(commands.Cog):
             )
         )
 
+    @autorole.command(name="set-bot", aliases=["set_bot"])
+    @commands.cooldown(3, 30, commands.BucketType.guild)
+    @commands.has_guild_permissions(manage_guild=True)
+    async def autorole_set_bot(
+        self, ctx: commands.Context, role: commands.RoleConverter
+    ):
+        """Sets the role given to all bots on join"""
+
+        config = ctx.bot.roles.get(ctx.guild.id, {})
+        config["autorole-bot"] = str(role.id)
+        ctx.bot.roles.put(ctx.guild.id, config)
+
+        await ctx.send(
+            embed=discord.Embed(
+                title="Autorole",
+                description=f"All bots will now get {role.mention} on join",
+            )
+        )
+
     @commands.Cog.listener()
     async def on_member_join(self, member: discord.Member):
         config = self.bot.roles.get(member.guild.id, {})
 
-        autorole = config.get("autorole", None)
+        autorole = config.get("autorole" if not member.bot else "autorole-bot", None)
 
         if autorole:
             await member.add_roles(discord.Object(id=int(autorole)))
